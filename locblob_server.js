c454450757a44df9c5ed29153b34646b8b498f36
Dennis Brandenburg <dennis.brandenburg@here.com>
2016-10-09 00:43:22 +0200

    proto to use package and locblob server

---
+++ locblob_server.js
@@ -0,0 +1,17 @@
+
+var grpc = require('grpc');
+var locblob = grpc.load('locblob.proto').locblob;
+
+function currentLocation(call, callback) {
+  callback(null, {message: call.request.longitute});
+}
+
+function main() {
+  var server = new grpc.Server();
+  server.addProtoService(locblob.CurrentLocationService.service,
+                         {currentLocation: currentLocation});
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
+}
+
+main();
